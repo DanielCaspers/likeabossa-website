6c23a5ba7f0af80a3a6b4cde97c8ad60bd8e8847
Daniel Caspers <daniel@caspersonline.us>
2016-04-17 18:40:11 -0500

    Refactored Gulp tasks to use DRY principle

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var runSequence = require('run-sequence');
 var uglify = require('gulp-uglify');
  
 var paths = {
-  scripts: ['js/*.js', '!js/*.min.js'],
+	scripts: ['js/*.js', '!js/*.min.js'],
 	libScripts: ['js/*.js', '!js/*.min.js',
 		'bower_components/jquery/dist/jquery.min.js' 
 	],
@@ -17,69 +17,58 @@ var paths = {
 	libStyles: ['bower_components/bootstrap/dist/css/bootstrap.min.css', 
 		'bower_components/font-awesome/css/font-awesome.min.css'
 	],
-  images: 'img/**/*'
+	images: 'img/**/*'
 };
  
 gulp.task('clean', function() {
-  return del(['dist']);
-});
- 
-gulp.task('minify:js:custom', ['clean'], function() {
-  // Minify and copy all homegrown JavaScript
-  return gulp.src(paths.scripts)
-      .pipe(uglify())
-      .pipe(concat('likeabossa.min.js'))
-    .pipe(gulp.dest('dist/js'));
+	return del(['dist']);
 });
 
-gulp.task('minify:js:lib', ['clean'], function() {
-  // Minify and copy all vendor JavaScript
-  return gulp.src(paths.libScripts)
-      .pipe(uglify())
-      .pipe(concat('lib.min.js'))
-    .pipe(gulp.dest('dist/js'));
-});
+function minifyJS(src, outputFileName, outputFolder){
+	return gulp.src(src)
+		.pipe(uglify())
+		.pipe(concat(outputFileName))
+		.pipe(gulp.dest(outputFolder));
+}
 
-gulp.task('minify:css:custom', ['clean'], function() {
-  // Minify and copy all homegrown styles
-  return gulp.src(paths.styles)
-      .pipe(cleanCSS({debug: true}, function(file) {
-				console.log(file.name + ': ' + file.stats.originalSize);
-				console.log(file.name + ': ' + file.stats.minifiedSize);
-			}))
-      .pipe(concat('likeabossa.min.css'))
-    .pipe(gulp.dest('dist/css'));
-});
+function minifyJSCustom(){
+	return minifyJS(paths.scripts, 'likeabossa.min.js', 'dist/js');
+}
 
-gulp.task('minify:css:lib', ['clean'], function() {
-  // Minify and copy all vendor styles
-  return gulp.src(paths.libStyles)
-      .pipe(cleanCSS({debug: true}, function(file) {
-				console.log(file.name + ': ' + file.stats.originalSize);
-				console.log(file.name + ': ' + file.stats.minifiedSize);
-			}))
-			.pipe(addsrc(paths.fonts))
-      .pipe(concat('lib.min.css'))
-    .pipe(gulp.dest('dist/css'));
-});
+function minifyJSLib(){
+	return minifyJS(paths.libScripts, 'lib.min.js', 'dist/js');
+}
+
+function minifyCSS(src, outputFileName){
+	return gulp.src(src)
+		.pipe(cleanCSS({debug: true}, function(file) {
+			console.log(file.name + ': ' + file.stats.originalSize);
+			console.log(file.name + ': ' + file.stats.minifiedSize);
+		}))
+		.pipe(concat(outputFileName))
+		.pipe(gulp.dest('dist/css'));
+}
+
+function minifyCSSCustom(){
+	return minifyCSS(paths.styles, 'likeabossa.min.css', 'dist/css');
+}
+
+function minifyCSSLib(){
+	return minifyCSS(paths.libStyles, 'lib.min.css', 'dist/css');
+}
+
+// Minify and copy all homegrown JavaScript
+gulp.task('minify:js:custom', ['clean'], minifyJSCustom);
+
+gulp.task('minify:js:lib', ['clean'], minifyJSLib);
+
+gulp.task('minify:css:custom', ['clean'], minifyCSSCustom);
+
+gulp.task('minify:css:lib', ['clean'], minifyCSSLib);
 
 gulp.task('minify:all', ['clean'], function(){
 	runSequence(['minify:css:custom', 'minify:css:lib', 'minify:js:custom', 'minify:js:lib']);
 })
- 
-// Copy all static images 
-/* gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    // Pass in options to the task 
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
-}); */
- 
-/* // Rerun the task when a file changes 
-gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.images, ['images']);
-}); */
- 
-//// The default task (called when you run `gulp` from cli) 
+  
+// The default task (called when you run `gulp` from cli) 
 gulp.task('default', ['clean', 'minify:all']);

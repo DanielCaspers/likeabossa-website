fe61b92dbd21bcf31b0fa5bb58d971c9964353ad
Daniel Caspers <daniel@caspersonline.us>
2016-04-17 17:45:46 -0500

    Dumping major changes into branch.

---
+++ js/IFrameLoaderAsync.js
@@ -0,0 +1,3 @@
+(function IframeLoaderAsync(){
+	document.getElementById('musicPlayer').src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/71933657&amp;color=fc9814&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false';	
+})
